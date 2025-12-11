a763dd2ede9eb3c6649315211736a6b1b305e418
marcinkawka <kawka.marcin@gmail.com>
2025-12-11 22:40:18 +0100

    another attempt

--- 2025/day11/graph02.py
+++ 2025/day11/graph02.py
@@ -1,5 +1,4 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from networkx.algorithms import all_simple_paths
 
 G = nx.DiGraph()
@@ -13,24 +12,18 @@ with open('input.txt', 'r') as f:
             G.add_node(node2)
             G.add_edge(node1, node2)
 
-all_paths = all_simple_paths(G, source='svr', target='out')
-
-print("All simple paths from 'svr' to 'out':")
-for path in all_paths:
-    if 'fft' in path and 'dac' in path:
-        print("Proper path:", path)
-        valid_paths.append(path)
-    counter+=1
-    if counter % 1000000 == 0:
-        print(f".",end='', flush=True)                  
-# print(f"There are in total {len(all_paths)} paths")
-
-# proper_paths = [path for path in all_paths if 'fft' in path and 'dac' in path]
-# for path in proper_paths:
-#     print("Proper path:", path)
-
-print(f"There are in total {len(valid_paths)} proper paths")
+def match_fft_dac(p):
+    return ('fft' in p) and ('dac' in p)
 
 
-# nx.draw(G, with_labels=True)
-# plt.show()
+for cutoff in range(1, 100):
+    all_paths = all_simple_paths(G, source='svr', target='out', cutoff=cutoff)
+    
+    path_count = 0
+    for path in all_paths:
+        path_count += 1
+        if match_fft_dac(path):
+            counter += 1
+    counter += path_count
+    if path_count>0:
+        print(f"with cutoff={cutoff} there are {path_count} paths {counter} total proper paths so far")
